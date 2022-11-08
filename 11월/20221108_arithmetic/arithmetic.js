f8b0fde85f4c459b1139cb2d64a52a2946b037d7
79ptke <82435745+79ptke@users.noreply.github.com>
2022-11-08 22:28:06 +0900

    20221108_두 수의 차이/ 두 수의 몫 구하기

---
+++ 11월/20221108_arithmetic/arithmetic.js
@@ -0,0 +1,14 @@
+// 두 수의 차
+function solution(num1, num2) {
+    let answer = num1 - num2;
+    return answer;
+}
+
+solution();
+
+function solution2(num1, num2) {
+    let answer = parseInt(num1 / num2);
+    return answer;
+}
+  
+solution2();
