76ea1eec737f3b4cd3c1be5996ea15d482110d4b
79ptke <82435745+79ptke@users.noreply.github.com>
2022-11-09 23:24:54 +0900

    20221109_ 두 수의 몫
    두 수의 몫을 구하는데에서 쓰인 메서드에 대한 보충 설명

--- 11월/20221108_arithmetic/arithmetic.js
+++ 11월/20221108_arithmetic/arithmetic.js
@@ -9,6 +9,8 @@ solution();
 // 두 수의 몫
 function solution2(num1, num2) {
     //let answer = parseInt(num1 / num2);
+    // 위의 것을 쓰면 양수일때는 상관이 없지만 음수가 나올 경우 
+    // 값이 달라지니 주의 해야한다.
     let answer = Math.floor(num1 / num2);
     return answer;
 }
