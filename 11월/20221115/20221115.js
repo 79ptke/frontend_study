e524795b4fd60862e5d1890624cfbe26b2be3b0c
79ptke <82435745+79ptke@users.noreply.github.com>
2022-11-17 23:06:10 +0900

    20221117_분수의 덧셈
    최소공약수를 구하는 부분에서 나타나는 오류 해결를 해결했다.

--- 11월/20221115/20221115.js
+++ 11월/20221115/20221115.js
@@ -6,20 +6,22 @@ function solution(deno1, num1, deno2, num2) {
     let deno = deno1*deno2; //분모
     let num = num1*deno2 + num2*deno1; //분자
 
-    console.log(num1 / deno1, num2 / deno2);
-    console.log(num / deno);
-    console.log(num, deno);
+    // console.log(num1 / deno1, num2 / deno2);
+    // console.log(num / deno);
+    // console.log(num, deno);
 
     let minNum;
-    // 약분
-    for(i = 0; i < deno; i++) {
-        if(deno%i === 0 && num%i === 0) {
-            minNum = i;
-            return[num/minNum, deno/minNum];
-        }
+    for(i=0; i < num; i++) { // 틀린 이유 1) i < deno(분모)가 아니라 i < num(분자)를 써야한다.
+        if(deno%i === 0 && num%i ===0) {
+            minNum = i;                  
+        } //틀린 이뉴2) for문이나 if문 안에 return을 하는게 아니라 밖에 써야한다.
     }
+    
+    // console.log(i);
+    // console.log(num, deno);
+    console.log(num/minNum , deno/minNum);
 
-    return [deno, num];
+    return [num/minNum , deno/minNum];  
 }
 
 $(document).ready(function() {
